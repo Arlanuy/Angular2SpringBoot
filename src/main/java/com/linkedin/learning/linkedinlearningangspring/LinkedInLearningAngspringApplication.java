e11c97727b996046761617d07edce8d0c69663d4
Arlan Uy <gnhs.com@gmail.com>
2021-09-29 14:37:55 -0500

    create Api config and initialized project with JPA H2 and String Web dependencies

---
+++ src/main/java/com/linkedin/learning/linkedinlearningangspring/LinkedInLearningAngspringApplication.java
@@ -0,0 +1,13 @@
+package com.linkedin.learning.linkedinlearningangspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedInLearningAngspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedInLearningAngspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linkedin/learning/linkedinlearningangspring/LinkedInLearningAngspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkedin.learning.linkedinlearningangspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedInLearningAngspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
